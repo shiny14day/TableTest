ce1fe578eec7da449512e6400b7652dea45f8c6a
coco <cocois@me.com>
2015-02-10 16:05:41 +0900

    camera + TableViewUI

---
+++ TableTest/FirstView.h
@@ -0,0 +1,23 @@
+//
+//  FirstView.h
+//  TableTest
+//
+//  Created by Pablo coco on 2015. 2. 8..
+//  Copyright (c) 2015년 Pablo coco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface FirstView : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *books;
+@property (nonatomic, strong) UIButton *navBar;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (BOOL) startCameraControllerFromViewController: (UIViewController*) controller
+                                   usingDelegate: (id <UIImagePickerControllerDelegate,
+                                                   UINavigationControllerDelegate>) delegate;
+@end
---
+++ TableTest/FirstView.m
@@ -0,0 +1,162 @@
+//
+//  FirstView.m
+//  TableTest
+//
+//  Created by Pablo coco on 2015. 2. 8..
+//  Copyright (c) 2015년 Pablo coco. All rights reserved.
+//
+
+#import "FirstView.h"
+#import "CameraView.h"
+
+
+@interface FirstView()
+
+@end
+
+@implementation FirstView
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    //테이블뷰 생성
+    CGFloat x = 0;
+    CGFloat y = 0;
+    CGFloat width = self.view.frame.size.width;
+    CGFloat height = self.view.frame.size.height;
+    CGRect tableFrame = CGRectMake(x, y, width, height);
+    UITableView *tableView = [[UITableView alloc]initWithFrame:tableFrame style:UITableViewStylePlain];
+    tableView.rowHeight = 120;
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
+    
+    //네비게이션용 뷰 생성
+    
+    
+    
+    //버튼 생성
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button addTarget:self
+               action:@selector(takeVideo)
+     forControlEvents:UIControlEventTouchDown];
+    [button setTitle:@"Menu" forState:UIControlStateNormal];
+    button.frame = CGRectMake(0.0, 10.0, 60.0, 50.0);
+    [self.view addSubview:button];
+
+}
+
+//hello
+
+-(IBAction)takeVideo{
+    [self startCameraControllerFromViewController: self
+                                    usingDelegate: self];
+}
+
+-(BOOL)startCameraControllerFromViewController:(UIViewController*)controller
+                                 usingDelegate:(id )delegate {
+    // 1 - Validattions
+    if (([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] == NO)
+        || (delegate == nil)
+        || (controller == nil)) {
+        return NO;
+    }
+    // 2 - Get image picker
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    // Displays a control that allows the user to choose movie capture
+    cameraUI.mediaTypes = [[NSArray alloc] initWithObjects:(NSString *)kUTTypeMovie, nil];
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    cameraUI.allowsEditing = NO;
+    cameraUI.delegate = delegate;
+    cameraUI.videoMaximumDuration = 1;
+    // 3 - Display image picker
+    [controller presentModalViewController: cameraUI animated: YES];
+    return YES;
+}
+
+
+// For responding to the user accepting a newly-captured picture or movie
+- (void) imagePickerController: (UIImagePickerController *) picker
+ didFinishPickingMediaWithInfo: (NSDictionary *) info {
+    
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    
+    [self dismissModalViewControllerAnimated:NO];
+    
+    // Handle a movie capture
+    if (CFStringCompare ((__bridge_retained CFStringRef) mediaType, kUTTypeMovie, 0)
+        == kCFCompareEqualTo) {
+        
+        NSString *moviePath = [[info objectForKey:
+                                UIImagePickerControllerMediaURL] path];
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath)) {
+            UISaveVideoAtPathToSavedPhotosAlbum (moviePath,self, @selector(video:didFinishSavingWithError:contextInfo:), nil);
+        }
+        
+        
+    }
+}
+
+- (void)video:(NSString*)videoPath didFinishSavingWithError:(NSError*)error contextInfo:(void*)contextInfo
+{
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Video Saving Failed"  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil, nil];
+        [alert show];
+    }else{
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"  delegate:self cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+        [alert show];
+    }
+}
+
+// Cancel버튼 눌렀을때
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissModalViewControllerAnimated: YES];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    else{
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+//    cell.textLabel.text=[NSString stringWithFormat:@"Cell %d",indexPath.row+1];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 150.0;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.books.count;
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
